542745402f02621ab2c4b0ac00ba45cef67bb50b
peter L <peter.l@uvaper.com>
2020-11-07 18:50:23 +0900

    react native study useref

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+  const nameInput = useRef();
 
-  const onChange = (e) => {
-    setText(e.target.value);
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+
+  const onChange = e => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
   };
 
   const onReset = () => {
-    setText('');
+    setInputs({
+      name: '',
+      nickname: ''
+    });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text}  />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
